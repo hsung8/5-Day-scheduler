6a1a08ecf9ca6ea4b54627342dd5aa20b20e7b40
macpro <haeseungsung@MacBook-Pro.local>
2020-07-18 12:11:22 -0400

    reduce redundancy

--- script.js
+++ script.js
@@ -2,18 +2,18 @@ var nowtime = moment().format("HH");
 var nowtimeInt = parseInt(nowtime);
 var saveBtn = $(".saveBtn");
 
-$("#9Row").attr("data-time", moment("9:00 am", "h:mm a").format("HH"));
-$("#10Row").attr("data-time", moment("10:00 am", "h:mm a").format("HH"));
-$("#11Row").attr("data-time", moment("11:00 am", "h:mm a").format("HH"));
-$("#12Row").attr("data-time", moment("12:00 pm", "h:mm a").format("HH"));
-$("#1Row").attr("data-time", moment("1:00 pm", "h:mm a").format("HH"));
-$("#2Row").attr("data-time", moment("2:00 pm", "h:mm a").format("HH"));
-$("#3Row").attr("data-time", moment("3:00 pm", "h:mm a").format("HH"));
-$("#4Row").attr("data-time", moment("4:00 pm", "h:mm a").format("HH"));
-$("#5Row").attr("data-time", moment("5:00 pm", "h:mm a").format("HH"));
-$("#6Row").attr("data-time", moment("6:00 pm", "h:mm a").format("HH"));
-$("#7Row").attr("data-time", moment("7:00 pm", "h:mm a").format("HH"));
-$("#8Row").attr("data-time", moment("8:00 pm", "h:mm a").format("HH"));
+$("#row-9").attr("data-time", moment("9:00 am", "h:mm a").format("HH"));
+$("#row-10").attr("data-time", moment("10:00 am", "h:mm a").format("HH"));
+$("#row-11").attr("data-time", moment("11:00 am", "h:mm a").format("HH"));
+$("#row-12").attr("data-time", moment("12:00 pm", "h:mm a").format("HH"));
+$("#row-1").attr("data-time", moment("1:00 pm", "h:mm a").format("HH"));
+$("#row-2").attr("data-time", moment("2:00 pm", "h:mm a").format("HH"));
+$("#row-3").attr("data-time", moment("3:00 pm", "h:mm a").format("HH"));
+$("#row-4").attr("data-time", moment("4:00 pm", "h:mm a").format("HH"));
+$("#row-5").attr("data-time", moment("5:00 pm", "h:mm a").format("HH"));
+$("#row-6").attr("data-time", moment("6:00 pm", "h:mm a").format("HH"));
+$("#row-7").attr("data-time", moment("7:00 pm", "h:mm a").format("HH"));
+$("#row-8").attr("data-time", moment("8:00 pm", "h:mm a").format("HH"));
 
 
 
@@ -29,17 +29,19 @@ $(document).ready(function () {
         var inputTimeInt = parseInt(inputTime);
         console.log(inputTimeInt);
 
+        var selector = $("#row-" + j)
+
         if (nowtimeInt === inputTimeInt){
-            $("#" + j + "Row").addClass("present");
+            selector.addClass("present");
             
         }
 
         if (nowtimeInt > inputTimeInt){
-            $("#" + j + "Row").addClass("past");
+            selector.addClass("past");
         }
 
         if (nowtimeInt < inputTimeInt){
-            $("#" + j + "Row").addClass("future");
+            selector.addClass("future");
         }
 
     }
@@ -47,7 +49,7 @@ $(document).ready(function () {
   
     saveBtn.on("click", function () {
         var hour = $(this).attr("data-hour");
-        var plan = $("#" + hour + "Row").val();
+        var plan = $("#row-" + hour).val();
         localStorage.setItem(hour, plan);
 
     });
@@ -55,7 +57,7 @@ $(document).ready(function () {
 
     function display() {
         for (var i = 1; i <= 12; i++) {
-            $("#" + i + "Row").val(localStorage.getItem(i));
+            $("#row-" + i).val(localStorage.getItem(i));
         }
     }
 
